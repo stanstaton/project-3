11fbc8d57af4e3cf1915a5bc4dcb6c14ecec6534
sabdi21 <sabdi21@hotmail.com>
2019-09-13 11:49:44 -0700

    removed node_mod

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Calendar from 'react-calendar';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Project 3 
+      </header>
+      <Calendar />
+    </div>
+  );
+}
+
+export default App;
