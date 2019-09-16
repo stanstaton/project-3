dead51e94f07431a5980245d2c5d7341175d22cd
sabdi21 <sabdi21@hotmail.com>
2019-09-16 14:35:17 -0700

    set change to start and end dated

--- client/src/form/Rent.js
+++ client/src/form/Rent.js
@@ -1,55 +1,41 @@
-import React from 'react'
+// import React from 'react'
+import React, { useState } from 'react';
 // import axios from 'axios'
 // import { Redirect } from 'react-router-dom'
-import { CustomInput, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { CustomInput, Form, FormGroup, Label} from 'reactstrap';
 import Rental from './Rental';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 class Rent extends React.Component {
-    // =============== calender stuff
-    state = {
-        startDate: new Date()
-      };
-     
-     handleDates = () => {
-        const [startDate, setStartDate] = useState(new Date("2019/09/18"));
-        const [endDate, setEndDate] = useState(new Date("2019/09/20"));
-        return (
-          <>
-            <DatePicker
-              selected={startDate}
-              onChange={date => setStartDate(date)}
-              selectsStart
-              startDate={startDate}
-              endDate={endDate}
-            />
-            <DatePicker
-              selected={endDate}
-              onChange={date => setEndDate(date)}
-              selectsEnd
-              endDate={endDate}
-              minDate={startDate}
-            />
-          </>
-        );
-      };
-
-     // =============== 
     constructor(props) {
         super(props)
         this.state = {
             currentNeighborhood: '',
             propertiesName: 'Property Name',
             image: 'https://placebear.com/200/300',
-        unAvailable: false
+            unAvailable: false,
+            startDate: new Date("09/17/2019"),
+            endDate: new Date("09/17/2019")
         }
     }
+     
+     handleDates = () => {
+         console.log('date has been choosen')
+
+      };
+      setStartDate = (date) => {
+          this.setState({startDate: date})
+      }
+      setEndDate = (date) => {
+        this.setState({endDate: date})
+    }
+     // =============== 
+
     handleRentalSubmit = (e) => {
         e.preventDefault()
-        // this.setState({ currentNeighborhood: })
+        this.setState({ currentNeighborhood: e.target.value})
         console.log('submitted!', e.target.value)
-        console.log('this is the neighborhood', this.state.currentNeighborhood)
         // axios.get()
         // .then()
         // .catch()
@@ -63,8 +49,7 @@ class Rent extends React.Component {
     render() {
 
     return (
-
-      <div className="page-header clear-filter" filter-color="blue">
+        <div className="page-header clear-filter" filter-color="blue">
         <div className="page-header-image" style={{ backgroundImage: "url(" + require("../assets/img/seattle.jpg") + ")" }}> </div>
         <div className='Rental-Form'>
         <h1>Rental Content</h1>
@@ -82,19 +67,29 @@ class Rent extends React.Component {
                 <option value="University District">University District</option>
             </CustomInput>
             </FormGroup>
-            <Button type="submit">Search Rental Properties!</Button>
+            <FormGroup>
+                <label>Select Start Date: </label>
+                <DatePicker
+                    selected={this.state.startDate}
+                    onChange={date => this.setStartDate(date)}
+                    selectsStart
+                    startDate={this.state.startDate}
+                    endDate={this.state.endDate}
+                />
+                <label>Select End Date:</label>
+                <DatePicker
+                    selected={this.state.endDate}
+                    onChange={date => this.setEndDate(date)}
+                    selectsEnd
+                    endDate={this.state.endDate}
+                    minDate={this.state.startDate} />
+            </FormGroup>
+            {/* <Button type="submit">Search Rental Properties!</Button> */}
         </Form>
         <Rental current={this.state.currentNeighborhood}/>
-        {/* pass data from the state into the rental component */}
+            <h2>{this.state.currentNeighborhood}</h2>
+            {/* <p>{this.state.startDate} thru {this.state.endDate}</p> */}
         </div>
-
-        {/* // ==================== */}
-        <DatePicker
-        selected={this.state.startDate}
-        onChange={this.handleChange} />
-        {/* // ====================  */}
-
-
         </div>
       
     )
